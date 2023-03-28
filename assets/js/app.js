8b68fbf1b403706f79830a62659fe0726f725357
MartinaDR1 <derosemartina933@gmail.com>
2023-03-28 15:10:37 +0200

    add text

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        message: 'Hello world!'
+      }
+    }
+}).mount('#app')
+
+
+
+
+
+
 /*Stampare a schermo un messaggio all’interno di un :puntare_a_destra: h1 :puntare_a_sinistra: utilizzando una proprietá nell'oggetto restituito dalla funzione data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
