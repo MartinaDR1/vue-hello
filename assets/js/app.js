9d07b45c22f416ba570e7e82834219b79480585d
MartinaDR1 <derosemartina933@gmail.com>
2023-03-28 15:23:28 +0200

    add bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        message: 'Hello world!'
+        message: 'Hello world!',
+        image: 'https://picsum.photos/200/300'
       }
     }
 }).mount('#app')
